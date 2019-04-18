71c4c07c5e667678cb3098d10b3c06701685eb91
Denys <denys.kostin@globallogic.com>
2019-04-18 18:40:34 +0300

    script improvements

--- build_meson.sh
+++ build_meson.sh
@@ -1,49 +1,52 @@
 #!/bin/bash
 
+filename=$(dirname "$0")/config
+test -f $filename || touch $filename
+source $(dirname "$0")/config
+if [ -z "$MESA_PATH" ]  ||  [ -z "$MESA_OUTPUT_PATH_x32" ]  ||  [ -z "$MESA_OUTPUT_PATH_x64" ]
+then
+    echo provide absolute path to local mesa repository
+    read MESA_PATH
+    echo provide desired pathes to x32 mesa version output
+    read MESA_OUTPUT_PATH_x32
+    echo provide desired pathes to x64 mesa version output
+    read MESA_OUTPUT_PATH_x64
+    echo "MESA_PATH=$MESA_PATH" >> $(dirname "$0")/config
+    echo "MESA_OUTPUT_PATH_x32=$MESA_OUTPUT_PATH_x32" >> $(dirname "$0")/config
+    echo "MESA_OUTPUT_PATH_x64=$MESA_OUTPUT_PATH_x64" >> $(dirname "$0")/config
+else
+    echo "Variables were taken from $(dirname "$0")/config file. If you want to change "
+fi
+
 #provides variables export according to selected architecture
 echo Select x32 or x64 mesa build
-
 read mesa_build
 
 if [ $mesa_build = x64 ]
 then
-    PATH_TO_LIB=/home/den/mesa64
-    export PKG_CONFIG_PATH=/home/den/work/xcb/out/1.13/lib/pkgconfig
+    PATH_TO_LIB=$MESA_OUTPUT_PATH_x64
     echo $PATH_TO_LIB
 elif [ $mesa_build = x32 ]
 then
-   PATH_TO_LIB=/home/den/mesa32
-   export PKG_CONFIG_PATH=(/home/den/work/x32/out/xcb_real/1.13/lib/pkgconfig:
-/home/den/work/x32/out/xcb/1.13/lib/pkgconfig:/home/den/work/x32/out/libdrm/lib/pkgconfig:
-/usr/lib/i386-linux-gnu/pkgconfig/)
+   PATH_TO_LIB=$MESA_OUTPUT_PATH_x32
    echo $PATH_TO_LIB
-   echo $PKG_CONFIG_PATH
 
 else
    echo "WRONG, START AGAIN"
    exit
 fi
 
-#provides folder name which includes git tag and current date
-echo Provide a folder name please
-
-read foldername
-cd /home/den/repositories/mesa/
+cd $MESA_PATH
+mesa_version=$(cat VERSION)
 
 mesa_hash=$(git rev-parse --short HEAD)
 current_date=$(date "+%d.%m")
-foldername1=$foldername-$mesa_hash-$current_date
+foldername1=$mesa_version-$mesa_hash-$current_date-DEBUG
 
 echo Library will be installed into $PATH_TO_LIB/$foldername1
 
 #checks for previously built folders and clean them if they exist and then builds libs
-if [ -d "mbuild64" ]
-then
-   rm -rf mbuild64/
-elif [ -d "mbuild32" ]
-then
-   rm -rf mbuild32/
-fi
+git clean -dfx
 
 if [ $mesa_build = x64 ]
 then
@@ -56,6 +59,7 @@ then
    -Dgallium-omx="disabled" \
    -Dplatforms=x11,drm,surfaceless \
    -Dtools=intel \
+   -Dd_ndebug=false \
    --prefix=$PATH_TO_LIB/$foldername1 ./mbuild64/
    ninja -C ./mbuild64/ install
 else
@@ -71,7 +75,16 @@ else
    -Dgallium-omx="disabled" \
    -Dplatforms=x11,drm,surfaceless \
    -Dtools=intel \
+   -Dd_ndebug=false \
    --prefix=$PATH_TO_LIB/$foldername1 ./mbuild32/
    ninja -C ./mbuild32/ install
 #set +x			# stop debugging from here
+fi
+
+echo "Do you want to export env variables? Type yes or no."
+read answer
+if [ $answer = yes ]
+then
+    export LD_PRELOAD=$(find $MESA_OUTPUT_PATH_x64/$foldername1/ -name libGL.so.1)
+    export LIBGL_DRIVERS_PATH=$(find $MESA_OUTPUT_PATH_x64/$foldername1/ -name dri)
 fi
